050bfe1bac07a7f082e333a79a6049c096325e9b
charan220 <raavicharan33@gmail.com>
2021-01-12 15:51:16 +0530

    [Charan]ADDED:calculates the premium and normal rides bonus anad validation for ids

---
+++ CabInvoiceGeneratorApp/CabInvoiceGenerator/CabInvoiceException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGeneratorApp.CabInvoiceGenerator
+{
+    public class CabInvoiceException   :Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_USER_ID,
+        }
+
+        public ExceptionType exceptionType;
+
+        public CabInvoiceException(string message, ExceptionType exceptionType)
+            : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
